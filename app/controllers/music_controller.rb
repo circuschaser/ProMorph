7e288d211d2933b619b9d3ba8ca8633f40a28ea0
circuschaser <rob@circuschaser.com>
2013-07-18 16:53:04 -0400

    musiccontroller

---
+++ app/controllers/music_controller.rb
@@ -0,0 +1,29 @@
+class MusicController < ApplicationController
+
+  # def index
+  #   @pieces = Piece.all
+  #   respond_to do |format|
+  #     format.html
+  #     format.xml { render :xml => @pieces}
+  #   end
+  # end
+  
+  # def new
+  #   @piece = Piece.new
+  #   respond_to do |format|
+  #     format.html
+  #   end
+  # end
+  
+  # def create
+  #   @piece = Piece.new(params[:piece])
+  #   respond_to do |format|
+  #     if @piece.save
+  #       format.html { redirect_to(admin_music_index_path) }
+  #     else
+  #       format.html { render :action => :new }
+  #     end
+  #   end
+  # end
+  
+end
